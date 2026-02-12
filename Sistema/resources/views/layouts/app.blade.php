1601b0cfb16df6bcf35ccb7425dae06d54ebc8a7
Nicolas Santos <santosnicolas026@gmail.com>
2026-02-12 14:18:18 -0300

    Atualização

--- Sistema/resources/views/layouts/app.blade.php
+++ Sistema/resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         .card-custom {
